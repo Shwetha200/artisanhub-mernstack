3ac374c5f64d1460c8e8ea0b33c7e5987e16878a
shwetha <skulal386@gmail.com>
2024-08-08 21:30:59 +0530

    .

--- client/src/components/shop/dashboardUSer/FetchApi.js
+++ client/src/components/shop/dashboardUSer/FetchApi.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 const apiURL = process.env.REACT_APP_API_URL;
 
-// import axios from "axios";
-// const apiURL = process.env.REACT_APP_API_URL;
-
 export const getUserById = async (uId) => {
   try {
     let res = await axios.post(`${apiURL}/api/user/signle-user`, { uId });
